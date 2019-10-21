c830570537efd1f22e4323f599ec049896c39a94
adinin319 <33631030+adinin319@users.noreply.github.com>
2019-10-20 20:24:32 -0400

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,338 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:\Users\andre\Documents\test1.py =================
+Would you like to start again?yes
+|---------{Mori Stat Card}---------|
+| Strength     = 16                |
+| Dexterity    = 12                |
+| Constitution = 16                |
+| Intelligence = 6                 |
+| Wisdom       = 10                |
+| Charisma     = 14                |
+|-------{Proficiency Skills}-------|
+| Saves: Strength, Constitution    |
+|                                  |
+| Skills: Animal Handling,         |
+| Athletic, Intimidation, Survival |
+|----------------------------------|
+| Acrobatics=1 | Animal Handling=2 |
+| Arcana=-2    | Athletics=5       |
+| Deception=2  | History=-2        |
+| Insight=0    | Intimidation=4    |
+| Invest=-2    | Medicine=0        |
+| Nature=-2    | Perception=0      |
+| Performance=2| Persuasion=2      |
+| Religion=-2  | Sleight of Hand=1 |
+| Stealth=1    | Survival=2        |
+|----------------------------------|
+What stat are you going rolling?
+1.)Ability Check
+2.)Attack Role
+3.)Basic Role
+1
+What sided dice are you rolling?3
+How many dice are you rolling?2
+3
+1
+|---------{Mori Stat Card}---------|
+| Strength     = 16                |
+| Dexterity    = 12                |
+| Constitution = 16                |
+| Intelligence = 6                 |
+| Wisdom       = 10                |
+| Charisma     = 14                |
+|-------{Proficiency Skills}-------|
+| Saves: Strength, Constitution    |
+|                                  |
+| Skills: Animal Handling,         |
+| Athletic, Intimidation, Survival |
+|----------------------------------|
+| Acrobatics=1 | Animal Handling=2 |
+| Arcana=-2    | Athletics=5       |
+| Deception=2  | History=-2        |
+| Insight=0    | Intimidation=4    |
+| Invest=-2    | Medicine=0        |
+| Nature=-2    | Perception=0      |
+| Performance=2| Persuasion=2      |
+| Religion=-2  | Sleight of Hand=1 |
+| Stealth=1    | Survival=2        |
+|----------------------------------|
+What stat are you going rolling?
+1.)Ability Check
+2.)Attack Role
+3.)Basic Role
+
+================= RESTART: C:\Users\andre\Documents\test1.py =================
+Would you like to start again?
+================= RESTART: C:\Users\andre\Documents\test1.py =================
+Would you like to start again?yes
+|---------{Mori Stat Card}---------|
+| Strength     = 16                |
+| Dexterity    = 12                |
+| Constitution = 16                |
+| Intelligence = 6                 |
+| Wisdom       = 10                |
+| Charisma     = 14                |
+|-------{Proficiency Skills}-------|
+| Saves: Strength, Constitution    |
+|                                  |
+| Skills: Animal Handling,         |
+| Athletic, Intimidation, Survival |
+|----------------------------------|
+| Acrobatics=1 | Animal Handling=2 |
+| Arcana=-2    | Athletics=5       |
+| Deception=2  | History=-2        |
+| Insight=0    | Intimidation=4    |
+| Invest=-2    | Medicine=0        |
+| Nature=-2    | Perception=0      |
+| Performance=2| Persuasion=2      |
+| Religion=-2  | Sleight of Hand=1 |
+| Stealth=1    | Survival=2        |
+|----------------------------------|
+What stat are you going rolling?
+1.)Ability Check
+2.)Attack Role
+3.)Basic Role
+Ability Check
+What stat are you rolling?(Please enter associated number)
+1.) Acrobatics
+2.) Animal Handling
+3.) Arcana
+4.) Athletics
+5.) Deception
+6.) History
+7.) Insight
+8.) Intimidation
+9.) Investigation
+10.) Medicine
+11.) Nature
+12.) Perception
+13.) Performance
+14.) Persuasion
+15.) Religion
+16.) Sleight of Hand
+17.) Stealth
+18.) Survival
+ Selection:Acrobatics
+You Rolled a : 5 
+The Total for Acrobatics Rolled is : 6
+|---------{Mori Stat Card}---------|
+| Strength     = 16                |
+| Dexterity    = 12                |
+| Constitution = 16                |
+| Intelligence = 6                 |
+| Wisdom       = 10                |
+| Charisma     = 14                |
+|-------{Proficiency Skills}-------|
+| Saves: Strength, Constitution    |
+|                                  |
+| Skills: Animal Handling,         |
+| Athletic, Intimidation, Survival |
+|----------------------------------|
+| Acrobatics=1 | Animal Handling=2 |
+| Arcana=-2    | Athletics=5       |
+| Deception=2  | History=-2        |
+| Insight=0    | Intimidation=4    |
+| Invest=-2    | Medicine=0        |
+| Nature=-2    | Perception=0      |
+| Performance=2| Persuasion=2      |
+| Religion=-2  | Sleight of Hand=1 |
+| Stealth=1    | Survival=2        |
+|----------------------------------|
+What stat are you going rolling?
+1.)Ability Check
+2.)Attack Role
+3.)Basic Role
+
+================= RESTART: C:\Users\andre\Documents\test1.py =================
+|---------{Mori Stat Card}---------|
+| Strength     = 16                |
+| Dexterity    = 12                |
+| Constitution = 16                |
+| Intelligence = 6                 |
+| Wisdom       = 10                |
+| Charisma     = 14                |
+|-------{Proficiency Skills}-------|
+| Saves: Strength, Constitution    |
+|                                  |
+| Skills: Animal Handling,         |
+| Athletic, Intimidation, Survival |
+|----------------------------------|
+| Acrobatics=1 | Animal Handling=2 |
+| Arcana=-2    | Athletics=5       |
+| Deception=2  | History=-2        |
+| Insight=0    | Intimidation=4    |
+| Invest=-2    | Medicine=0        |
+| Nature=-2    | Perception=0      |
+| Performance=2| Persuasion=2      |
+| Religion=-2  | Sleight of Hand=1 |
+| Stealth=1    | Survival=2        |
+|----------------------------------|
+What stat are you going rolling?
+1.)Ability Check
+2.)Attack Role
+3.)Basic Role
+Basic Role
+What sided dice are you rolling?3
+How many dice are you rolling?2
+2
+1
+>>> main()
+|---------{Mori Stat Card}---------|
+| Strength     = 16                |
+| Dexterity    = 12                |
+| Constitution = 16                |
+| Intelligence = 6                 |
+| Wisdom       = 10                |
+| Charisma     = 14                |
+|-------{Proficiency Skills}-------|
+| Saves: Strength, Constitution    |
+|                                  |
+| Skills: Animal Handling,         |
+| Athletic, Intimidation, Survival |
+|----------------------------------|
+| Acrobatics=1 | Animal Handling=2 |
+| Arcana=-2    | Athletics=5       |
+| Deception=2  | History=-2        |
+| Insight=0    | Intimidation=4    |
+| Invest=-2    | Medicine=0        |
+| Nature=-2    | Perception=0      |
+| Performance=2| Persuasion=2      |
+| Religion=-2  | Sleight of Hand=1 |
+| Stealth=1    | Survival=2        |
+|----------------------------------|
+What stat are you going rolling?
+1.)Ability Check
+2.)Attack Role
+3.)Basic Role
+
+================= RESTART: C:\Users\andre\Documents\test1.py =================
+Traceback (most recent call last):
+  File "C:\Users\andre\Documents\test1.py", line 264, in <module>
+    main()
+NameError: name 'main' is not defined
+>>> 
+================= RESTART: C:\Users\andre\Documents\test1.py =================
+|---------{Mori Stat Card}---------|
+|(enter reset() to restart program)|
+| Strength     = 16                |
+| Dexterity    = 12                |
+| Constitution = 16                |
+| Intelligence = 6                 |
+| Wisdom       = 10                |
+| Charisma     = 14                |
+|-------{Proficiency Skills}-------|
+| Saves: Strength, Constitution    |
+|                                  |
+| Skills: Animal Handling,         |
+| Athletic, Intimidation, Survival |
+|----------------------------------|
+| Acrobatics=1 | Animal Handling=2 |
+| Arcana=-2    | Athletics=5       |
+| Deception=2  | History=-2        |
+| Insight=0    | Intimidation=4    |
+| Invest=-2    | Medicine=0        |
+| Nature=-2    | Perception=0      |
+| Performance=2| Persuasion=2      |
+| Religion=-2  | Sleight of Hand=1 |
+| Stealth=1    | Survival=2        |
+|----------------------------------|
+What stat are you going rolling?
+1.)Ability Check
+2.)Attack Role
+3.)Basic Role
+
+================= RESTART: C:\Users\andre\Documents\test1.py =================
+>>> reset
+<function reset at 0x03BF60C0>
+>>> reset()
+|---------{Mori Stat Card}---------|
+|(enter reset() to restart program)|
+| Strength     = 16                |
+| Dexterity    = 12                |
+| Constitution = 16                |
+| Intelligence = 6                 |
+| Wisdom       = 10                |
+| Charisma     = 14                |
+|-------{Proficiency Skills}-------|
+| Saves: Strength, Constitution    |
+|                                  |
+| Skills: Animal Handling,         |
+| Athletic, Intimidation, Survival |
+|----------------------------------|
+| Acrobatics=1 | Animal Handling=2 |
+| Arcana=-2    | Athletics=5       |
+| Deception=2  | History=-2        |
+| Insight=0    | Intimidation=4    |
+| Invest=-2    | Medicine=0        |
+| Nature=-2    | Perception=0      |
+| Performance=2| Persuasion=2      |
+| Religion=-2  | Sleight of Hand=1 |
+| Stealth=1    | Survival=2        |
+|----------------------------------|
+What stat are you going rolling?
+1.)Ability Check
+2.)Attack Role
+3.)Basic Role
+
+================= RESTART: C:\Users\andre\Documents\test1.py =================
+|---------{Mori Stat Card}---------|
+|(enter reset() to restart program)|
+| Strength     = 16                |
+| Dexterity    = 12                |
+| Constitution = 16                |
+| Intelligence = 6                 |
+| Wisdom       = 10                |
+| Charisma     = 14                |
+|-------{Proficiency Skills}-------|
+| Saves: Strength, Constitution    |
+|                                  |
+| Skills: Animal Handling,         |
+| Athletic, Intimidation, Survival |
+|----------------------------------|
+| Acrobatics=1 | Animal Handling=2 |
+| Arcana=-2    | Athletics=5       |
+| Deception=2  | History=-2        |
+| Insight=0    | Intimidation=4    |
+| Invest=-2    | Medicine=0        |
+| Nature=-2    | Perception=0      |
+| Performance=2| Persuasion=2      |
+| Religion=-2  | Sleight of Hand=1 |
+| Stealth=1    | Survival=2        |
+|----------------------------------|
+What stat are you going rolling?
+1.)Ability Check
+2.)Attack Role
+3.)Basic Role
+35
+Please print the proper answer
+>>> reset()
+|---------{Mori Stat Card}---------|
+|(enter reset() to restart program)|
+| Strength     = 16                |
+| Dexterity    = 12                |
+| Constitution = 16                |
+| Intelligence = 6                 |
+| Wisdom       = 10                |
+| Charisma     = 14                |
+|-------{Proficiency Skills}-------|
+| Saves: Strength, Constitution    |
+|                                  |
+| Skills: Animal Handling,         |
+| Athletic, Intimidation, Survival |
+|----------------------------------|
+| Acrobatics=1 | Animal Handling=2 |
+| Arcana=-2    | Athletics=5       |
+| Deception=2  | History=-2        |
+| Insight=0    | Intimidation=4    |
+| Invest=-2    | Medicine=0        |
+| Nature=-2    | Perception=0      |
+| Performance=2| Persuasion=2      |
+| Religion=-2  | Sleight of Hand=1 |
+| Stealth=1    | Survival=2        |
+|----------------------------------|
+What stat are you going rolling?
+1.)Ability Check
+2.)Attack Role
+3.)Basic Role
